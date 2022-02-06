7aaf96e231cefb87b5e56d0ae677bb90766f6737
Vincent Leong <contact@leviwaiu.com>
2022-02-06 23:48:23 +0000

    Attempt to introduce ico.

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/contact":["static\u002Fchunks\u002Fpages\u002Fcontact.js"],"/cv":["static\u002Fchunks\u002Fpages\u002Fcv.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcontact","\u002Fcv"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
